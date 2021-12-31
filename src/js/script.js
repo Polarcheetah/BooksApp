aa1ac3bdf77afc51e351fc626649d72572f10dda
Maria Berk <marysia.berk@gmail.com>
2021-12-31 14:11:04 +0100

    Add function filterBook

--- src/js/script.js
+++ src/js/script.js
@@ -21,6 +21,13 @@
     ),
   };
 
+  const classNames = {
+    bookList: {
+      bookImage: 'book__image',
+      favoriteBook: 'favorite',
+    },
+  };
+
   function render() {
     for (let book of dataSource.books) {
       /* generate HTML based on template*/
@@ -40,26 +47,33 @@
     //const allBooks = document.querySelectorAll(select.all.books);
     const booksList = document.querySelector(select.containerOf.books);
 
-    //console.log(booksList);
+    //console.log(booksList.innerHTML);
 
     booksList.addEventListener('dblclick', function (event) {
-      if (event.target.offsetParent.classList.contains('book__image')) {
+      if (
+        event.target.offsetParent.classList.contains(
+          classNames.bookList.bookImage
+        )
+      ) {
         event.preventDefault();
         console.log('element clicked!');
-        event.target.offsetParent.classList.add('favorite');
+        event.target.offsetParent.classList.add(
+          classNames.bookList.favoriteBook
+        );
         const bookId = event.target.offsetParent.getAttribute('data-id');
         if (!favoriteBooks.includes(bookId)) {
           favoriteBooks.push(bookId);
         } else {
           const index = favoriteBooks.indexOf(bookId);
           favoriteBooks.splice(index, 1);
-          event.target.offsetParent.classList.remove('favorite');
+          event.target.offsetParent.classList.remove(
+            classNames.bookList.favoriteBook
+          );
         }
         console.log(favoriteBooks);
       }
     });
 
-    const filters = [];
     const filtersForm = document.querySelector(select.containerOf.filters);
     console.log(filters, filtersForm);
     filtersForm.addEventListener('click', function (event) {
@@ -78,9 +92,29 @@
           filters.splice(indexOfUnchecked, 1);
         }
       }
-      console.log(filters);
+      console.log('filters:', filters);
+      filterBooks();
     });
   }
+
+  function filterBooks() {
+    for (let book of dataSource.books) {
+      console.log(book);
+      const hiddenBook = document.querySelector(
+        `.book__image[data-id="${book.id}"]`
+      );
+      if (
+        (filters.includes('adults') && book.details.adults === false) ||
+        (filters.includes('nonFiction') && book.details.nonFiction === false)
+      ) {
+        hiddenBook.classList.add('hidden');
+      } else {
+        hiddenBook.classList.remove('hidden');
+      }
+    }
+  }
+
+  const filters = [];
 
   render();
   initAction();
